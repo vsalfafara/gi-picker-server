fe376d3c7bd7a1620f3b57f8e12527d66b0d5b51
vsalfafara <alfafara.vm@gmail.com>
2023-05-26 18:05:22 +0800

    Disabled sheet

--- src/server.ts
+++ src/server.ts
@@ -1,204 +1,226 @@
-import 'dotenv/config';
-import express from 'express'
-import http from 'http'
-import cors from 'cors'
-import { Server } from 'socket.io'
-import { v4 as uuidv4 } from 'uuid'
-import { User, Character, Sequence } from './types'
-import { getAllUsers, userJoin, getUser, getAllPlayersInRoom, removeUser, getHostInRoom } from './users'
-import { createGame, getGame, getPlayers, getTurn, increasePointer, removeGame, resetSequence, setPlayers, setSequence } from './games';
-import {saveData, getData } from './sheets';
-import type { Request, Response } from 'express'
+import "dotenv/config";
+import express from "express";
+import http from "http";
+import cors from "cors";
+import { Server } from "socket.io";
+import { v4 as uuidv4 } from "uuid";
+import { User, Character, Sequence } from "./types";
+import {
+  getAllUsers,
+  userJoin,
+  getUser,
+  getAllPlayersInRoom,
+  removeUser,
+  getHostInRoom,
+} from "./users";
+import {
+  createGame,
+  getGame,
+  getPlayers,
+  getTurn,
+  increasePointer,
+  removeGame,
+  resetSequence,
+  setPlayers,
+  setSequence,
+} from "./games";
+// import { saveData, getData } from "./sheets";
+import type { Request, Response } from "express";
 
-const app = express()
-const server = http.createServer(app)
+const app = express();
+const server = http.createServer(app);
 // const key = process.env.SECRET
 
 const io = new Server(server, {
   cors: {
-    origin: "*"
-  }
-})
+    origin: "*",
+  },
+});
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 
-app.use(cors())
-app.use(express.urlencoded({
-  extended: true
-}));
+app.use(cors());
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
 app.use(express.json());
 
-app.get('/', (_: Request, res: Response) => {
-  res.sendStatus(200)
-})
+app.get("/", (_: Request, res: Response) => {
+  res.sendStatus(200);
+});
 
-app.post('/saveData', async (req: Request, res: Response) => {
-  const response = await saveData(req.body)
-  res.sendStatus(response.status)
-})
+// app.post('/saveData', async (req: Request, res: Response) => {
+//   const response = await saveData(req.body)
+//   res.sendStatus(response.status)
+// })
 
-app.get('/getData/:mode', async (req: Request, res: Response) => {
-  const response = await getData(req.params.mode)
-  res.json(response.data.values.length + 1)
-})
+// app.get('/getData/:mode', async (req: Request, res: Response) => {
+//   const response = await getData(req.params.mode)
+//   res.json(response.data.values.length + 1)
+// })
 
-io.on('connection', (socket) => {
-  socket.removeAllListeners()
-  socket.on('createRoom', (data: { name: string }) => {
+io.on("connection", (socket) => {
+  socket.removeAllListeners();
+  socket.on("createRoom", (data: { name: string }) => {
     const user: User = {
       id: socket.id,
       name: data.name,
       roomId: uuidv4(),
-      isHost: true
-    }
-    userJoin(user)
-    socket.join(user.roomId)
-    socket.emit('setUser', user)
-    socket.emit('getRoomId', user.roomId)
-  })
-  socket.on('joinRoom', (data: {name: string, roomId: string}) => {
+      isHost: true,
+    };
+    userJoin(user);
+    socket.join(user.roomId);
+    socket.emit("setUser", user);
+    socket.emit("getRoomId", user.roomId);
+  });
+  socket.on("joinRoom", (data: { name: string; roomId: string }) => {
     const user: User = {
       id: socket.id,
       name: data.name,
       roomId: data.roomId,
-      isHost: false
-    }
-    userJoin(user)
-    socket.join(user.roomId)
-    socket.emit('getRoomId', user.roomId)
-    socket.emit('setUser', user)
-    const players = getAllPlayersInRoom(user.roomId)
-    io.in(user.roomId).emit('getAllPlayersInRoom', players)
-  })
-  
-  socket.on('rejoinRoom', (user?: User) => {
+      isHost: false,
+    };
+    userJoin(user);
+    socket.join(user.roomId);
+    socket.emit("getRoomId", user.roomId);
+    socket.emit("setUser", user);
+    const players = getAllPlayersInRoom(user.roomId);
+    io.in(user.roomId).emit("getAllPlayersInRoom", players);
+  });
+
+  socket.on("rejoinRoom", (user?: User) => {
     if (user) {
-      const userExists = getUser(socket.id)
+      const userExists = getUser(socket.id);
       if (!userExists) {
-        user.id = socket.id
-        userJoin(user)
-        socket.join(user.roomId)
-        socket.emit('setUser', user)
-        const players = getAllPlayersInRoom(user.roomId)
-        io.in(user.roomId).emit('getAllPlayersInRoom', players)
+        user.id = socket.id;
+        userJoin(user);
+        socket.join(user.roomId);
+        socket.emit("setUser", user);
+        const players = getAllPlayersInRoom(user.roomId);
+        io.in(user.roomId).emit("getAllPlayersInRoom", players);
       }
     }
-  })
+  });
 
-  socket.on('getAllPlayersInRoom', (roomId: string) => {
-    const players = getAllPlayersInRoom(roomId)
-    io.in(roomId).emit('getAllPlayersInRoom', players)
-  })
+  socket.on("getAllPlayersInRoom", (roomId: string) => {
+    const players = getAllPlayersInRoom(roomId);
+    io.in(roomId).emit("getAllPlayersInRoom", players);
+  });
 
-  socket.on('getUser', () => {
-    const user = getUser(socket.id)
-    socket.emit('setUser', user)
-  })
+  socket.on("getUser", () => {
+    const user = getUser(socket.id);
+    socket.emit("setUser", user);
+  });
 
-  socket.on('getAllPlayersInGame', (roomId: string) => {
-    const players = getPlayers(roomId)
-    socket.emit('getAllPlayersInGame', players)
-  })
-    
-  let time: number = null
-  let timeHandler: ReturnType<typeof setInterval>
+  socket.on("getAllPlayersInGame", (roomId: string) => {
+    const players = getPlayers(roomId);
+    socket.emit("getAllPlayersInGame", players);
+  });
 
-  socket.on('startGame', (data: any) => {
-    createGame(data)
-    resetSequence(data.roomId)
+  let time: number = null;
+  let timeHandler: ReturnType<typeof setInterval>;
+
+  socket.on("startGame", (data: any) => {
+    createGame(data);
+    resetSequence(data.roomId);
     if (data.firstPick) {
-      data.players = [data.players[1], data.players[0]]
+      data.players = [data.players[1], data.players[0]];
     }
-    setPlayers(data.roomId, data.players)
-    const selection = setSequence(data.roomId, data.gameType, data.mode)
-    data.game = getGame(data.roomId)
-    data.game.mode = data.mode
-    data.noOfPicks = Number(selection.noOfPicks)
-    data.noOfBans = Number(selection.noOfBans)
-    io.to(data.players[0].id).emit('setPlayer', 0)
-    io.to(data.players[1].id).emit('setPlayer', 1)
-    io.in(data.roomId).emit('startGame', data)
-  })
+    setPlayers(data.roomId, data.players);
+    const selection = setSequence(data.roomId, data.gameType, data.mode);
+    data.game = getGame(data.roomId);
+    data.game.mode = data.mode;
+    data.noOfPicks = Number(selection.noOfPicks);
+    data.noOfBans = Number(selection.noOfBans);
+    io.to(data.players[0].id).emit("setPlayer", 0);
+    io.to(data.players[1].id).emit("setPlayer", 1);
+    io.in(data.roomId).emit("startGame", data);
+  });
 
-  socket.on('draftStart', (roomId: string) => {
-    io.in(roomId).emit('draftStart')
-  })
+  socket.on("draftStart", (roomId: string) => {
+    io.in(roomId).emit("draftStart");
+  });
 
-  socket.on('nextTurn', (roomId: string) => {
-    clearInterval(timeHandler)
-    increasePointer(roomId)
-    const turn = getTurn(roomId)
+  socket.on("nextTurn", (roomId: string) => {
+    clearInterval(timeHandler);
+    increasePointer(roomId);
+    const turn = getTurn(roomId);
     if (turn) {
-      io.in(roomId).emit('announceTurn', turn)
-      io.to(turn.player.id).emit('select', turn.selection)
+      io.in(roomId).emit("announceTurn", turn);
+      io.to(turn.player.id).emit("select", turn.selection);
     } else {
-      io.in(roomId).emit('gameCompleted')
+      io.in(roomId).emit("gameCompleted");
     }
-  })
+  });
 
-  socket.on('stopTimer', () => {
-    clearInterval(timeHandler)
-  })
+  socket.on("stopTimer", () => {
+    clearInterval(timeHandler);
+  });
 
-  socket.on('startTimer', (roomId: string) => {
-    const game = getGame(roomId)
+  socket.on("startTimer", (roomId: string) => {
+    const game = getGame(roomId);
     if (game.withTimer) {
-      const turn: Sequence | boolean = getTurn(roomId)
+      const turn: Sequence | boolean = getTurn(roomId);
       if (turn) {
-        time = game.time
+        time = game.time;
         timeHandler = setInterval(() => {
-          time--
-          io.to(roomId).emit('getTime', time)
+          time--;
+          io.to(roomId).emit("getTime", time);
           if (time === 0) {
-            clearInterval(timeHandler)
-            io.to(turn.player.id).emit('noPick', turn)
+            clearInterval(timeHandler);
+            io.to(turn.player.id).emit("noPick", turn);
           }
-        }, 1000)
+        }, 1000);
       }
     }
-  })
+  });
 
-  socket.on('removeCharacterFromPanel', (data: {character: Character, roomId: string }) => {
-    io.in(data.roomId).emit('removeCharacterFromPanel', data)
-  })
-
-  socket.on('setNewSelection', (data: any) => {
-    io.in(data.roomId).emit('setNewSelection', data)
-  })
-
-  socket.on('goBack', (roomId: string) => {
-    removeGame(roomId)
-    io.in(roomId).emit('goBack')
-  })
-
-  socket.on('removeUser', () => {
-    const user = getUser(socket.id)
-    if (user) {
-      removeUser(user.id)
+  socket.on(
+    "removeCharacterFromPanel",
+    (data: { character: Character; roomId: string }) => {
+      io.in(data.roomId).emit("removeCharacterFromPanel", data);
     }
-  })
+  );
 
-  socket.on('chat', (data: any) => {
-    socket.broadcast.emit('chat', data)
-  })
+  socket.on("setNewSelection", (data: any) => {
+    io.in(data.roomId).emit("setNewSelection", data);
+  });
 
-  socket.on('setSheetData', ({sheetData, roomId, colReference}) => {
-    io.in(roomId).emit('setSheetData', ({sheetData, colReference}))
-  })
+  socket.on("goBack", (roomId: string) => {
+    removeGame(roomId);
+    io.in(roomId).emit("goBack");
+  });
 
-  socket.on('disconnect', () => {
-    const user = getUser(socket.id)
+  socket.on("removeUser", () => {
+    const user = getUser(socket.id);
     if (user) {
-      removeUser(user.id)
-      socket.leave(user.roomId)
-      const players = getAllPlayersInRoom(user.roomId)
-      io.to(user.roomId).emit('getAllPlayersInRoom', players)
-      io.to(user.roomId).emit('disconnected', user.name)
+      removeUser(user.id);
     }
-    console.log('user disconnected')
-  })
-})
+  });
+
+  socket.on("chat", (data: any) => {
+    socket.broadcast.emit("chat", data);
+  });
+
+  socket.on("setSheetData", ({ sheetData, roomId, colReference }) => {
+    io.in(roomId).emit("setSheetData", { sheetData, colReference });
+  });
+
+  socket.on("disconnect", () => {
+    const user = getUser(socket.id);
+    if (user) {
+      removeUser(user.id);
+      socket.leave(user.roomId);
+      const players = getAllPlayersInRoom(user.roomId);
+      io.to(user.roomId).emit("getAllPlayersInRoom", players);
+      io.to(user.roomId).emit("disconnected", user.name);
+    }
+    console.log("user disconnected");
+  });
+});
 
 server.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`)
-})
+  console.log(`Server is running on port ${PORT}`);
+});
--- src/sheets.ts
+++ src/sheets.ts
@@ -1,82 +1,82 @@
-import { google } from "googleapis"
-import { format } from "date-fns"
+// import { google } from "googleapis"
+// import { format } from "date-fns"
 
-const abyssSpreadsheetId = process.env.SHEET_ID_ABYSS
-const amberGamesSpreadsheetId = process.env.SHEET_ID_AMBER_GAMES
-const kotSpreadsheetId = process.env.SHEET_ID_KOT
+// const abyssSpreadsheetId = process.env.SHEET_ID_ABYSS
+// const amberGamesSpreadsheetId = process.env.SHEET_ID_AMBER_GAMES
+// const kotSpreadsheetId = process.env.SHEET_ID_KOT
 
-type Sheet = {
-  mode: string
-  sheetId: string
-}
+// type Sheet = {
+//   mode: string
+//   sheetId: string
+// }
 
-const sheets: Sheet[] = [
-  {
-    mode: "amberGames",
-    sheetId: amberGamesSpreadsheetId,
-  },
-  {
-    mode: "kingOfTeyvat",
-    sheetId: kotSpreadsheetId,
-  },
-  {
-    mode: "abyss",
-    sheetId: abyssSpreadsheetId,
-  },
-]
+// const sheets: Sheet[] = [
+//   {
+//     mode: "amberGames",
+//     sheetId: amberGamesSpreadsheetId,
+//   },
+//   {
+//     mode: "kingOfTeyvat",
+//     sheetId: kotSpreadsheetId,
+//   },
+//   {
+//     mode: "abyss",
+//     sheetId: abyssSpreadsheetId,
+//   },
+// ]
 
-function getSheetId(mode: string) {
-  const sheetData = sheets.find((sheet: Sheet) => sheet.mode === mode)
-  return sheetData.sheetId
-}
+// function getSheetId(mode: string) {
+//   const sheetData = sheets.find((sheet: Sheet) => sheet.mode === mode)
+//   return sheetData.sheetId
+// }
 
-export async function saveData(data: any) {
-  const { sheetData, mode } = data
-  sheetData.unshift(format(new Date(), "MM/dd/yyyy h:mm b"))
+// export async function saveData(data: any) {
+//   const { sheetData, mode } = data
+//   sheetData.unshift(format(new Date(), "MM/dd/yyyy h:mm b"))
 
-  const auth = new google.auth.GoogleAuth({
-    keyFile: "secrets.json",
-    scopes: "https://www.googleapis.com/auth/spreadsheets",
-  })
+//   const auth = new google.auth.GoogleAuth({
+//     keyFile: "secrets.json",
+//     scopes: "https://www.googleapis.com/auth/spreadsheets",
+//   })
 
-  const client = await auth.getClient()
+//   const client = await auth.getClient()
 
-  const sheets = google.sheets({
-    version: "v4",
-    auth: client,
-  })
+//   const sheets = google.sheets({
+//     version: "v4",
+//     auth: client,
+//   })
 
-  const response = await sheets.spreadsheets.values.append({
-    auth,
-    spreadsheetId: getSheetId(mode),
-    range: "History",
-    valueInputOption: "USER_ENTERED",
-    requestBody: {
-      values: [sheetData],
-    },
-  })
+//   const response = await sheets.spreadsheets.values.append({
+//     auth,
+//     spreadsheetId: getSheetId(mode),
+//     range: "History",
+//     valueInputOption: "USER_ENTERED",
+//     requestBody: {
+//       values: [sheetData],
+//     },
+//   })
 
-  return response
-}
+//   return response
+// }
 
-export async function getData(mode: string) {
-  const auth = new google.auth.GoogleAuth({
-    keyFile: "secrets.json",
-    scopes: "https://www.googleapis.com/auth/spreadsheets",
-  })
+// export async function getData(mode: string) {
+//   const auth = new google.auth.GoogleAuth({
+//     keyFile: "secrets.json",
+//     scopes: "https://www.googleapis.com/auth/spreadsheets",
+//   })
 
-  const client = await auth.getClient()
+//   const client = await auth.getClient()
 
-  const sheets = google.sheets({
-    version: "v4",
-    auth: client,
-  })
+//   const sheets = google.sheets({
+//     version: "v4",
+//     auth: client,
+//   })
 
-  const response = await sheets.spreadsheets.values.get({
-    auth,
-    spreadsheetId: getSheetId(mode),
-    range: "History",
-  })
+//   const response = await sheets.spreadsheets.values.get({
+//     auth,
+//     spreadsheetId: getSheetId(mode),
+//     range: "History",
+//   })
 
-  return response
-}
+//   return response
+// }
